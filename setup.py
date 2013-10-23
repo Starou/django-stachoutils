37fefd141d38f49e8641e94d4174d6ada736ceb8
Stanislas Guerra <stanislas.guerra@gmail.com>
2013-10-23 15:59:24 +0200

    Change package name.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
 setup(
-    name="Stachou Utils",
+    name="django-stachoutils",
     version="0.90",
     license='BSD Licence',
     author='Stanislas Guerra',
