79a03343b01da0a4f31122fe08323a5bbc6f749f
Stanislas Guerra <stanislas.guerra@gmail.com>
2013-10-23 15:39:24 +0200

    clean-fk-widget - remover RelatedObjectLookups.js override. Clean js and css related to the widget.

--- django_stachoutils/static/django_stachoutils/js/widgets.js
+++ django_stachoutils/static/django_stachoutils/js/widgets.js
@@ -1,27 +1,3 @@
-// TODO Faire un namespace Adencf.
-
-function onForeignKeyChanged(elem, callback, get_label_url) {
-    var app_label = elem.siblings('.related_app_name').val();
-    var model_label = elem.siblings('.related_model_name').val();
-    var show_related_lookup = elem.siblings('.show_related_lookup').val();
-    var onclick  = 'return false;';
-    if (show_related_lookup == '1') onclick = "return showRelatedObjectPopup(this);";
-    if (elem.val() && get_label_url) {
-        var url = get_label_url;
-        url = url + '?pk='+elem.val();
-        $.getJSON(url, {}, function(data) { // màj du label > lien vers la fk.
-            var link = $('<a class="preview" href="'+data['URL']+'" onclick="'+onclick+'">'+data['LABEL']+'</a>');
-            if (data.PREVIEW_URL) {
-                var preview = $('<img src="'+data.PREVIEW_URL+'" />');
-                preview.appendTo(link);
-            }
-            elem.siblings('div.label_container').find('strong.label').html(link);
-            if (callback) callback(elem); 
-        });
-    } else if (callback) callback(elem);  // Clear du rawId.
-    else elem.parent().find('.label').html('&nbsp;');
-}
-
 $(document).ready(function () {
     $(".editeur").each(function () {
         var textArea = $(this).find("textarea");
@@ -43,13 +19,5 @@ $(document).ready(function () {
         var input = $(this).parent().find("input:text");
         var selection = input.getSelection().text;
         input.replaceSelection(selection.toLowerCase(), true);
-    });
-    
-    /* Initialise la position des preview à côté du lien pour le mouseover. */
-    $(document).delegate('.field-box p.preview a, a.preview', 'mouseover', function(){
-        var pos = $(this).position();    
-        var left = (pos.left + 5) + "px";
-        var top = (pos.top - 55)+ "px";
-        $(this).children().css({ left: left, top: top});
     });
 });
