e27405e955a3da44187ba960ac4067a40464c643
Stanislas Guerra <stanislas.guerra@gmail.com>
2013-10-29 13:01:05 +0100

    admin-extras - stashed.

--- tests/projects/django_project/my_admin.py
+++ tests/projects/django_project/my_admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.contrib.admin.templatetags.admin_static import static
+from django_stachoutils.admin import ModelAdmin
 from my_app import models
 
 
 class MyAdmin(admin.AdminSite):
     pass
 
+
+class 
     
 
 admin_site = MyAdmin(name='my-admin')
