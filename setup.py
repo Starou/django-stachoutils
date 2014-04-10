d679b05673f843bf5f0f2c12652656e097808aaa
Stanislas Guerra <stanislas.guerra@gmail.com>
2014-04-10 12:09:31 +0200

    Fix installation via python setup.py install. Simplification de package_data dans setup.py. Les static sont listés dans MANIFEST.in. utilisation de find_packages() pour que ça marche quand on fait un python setup.py install.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 from distutils.core import setup
+from setuptools import find_packages
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
 
@@ -9,31 +10,15 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-stachoutils",
-    version="0.91.4",
+    version="0.91.5",
     license='BSD Licence',
     author='Stanislas Guerra',
     author_email='stan@slashdev.me',
     description='Commons for Django',
     url='https://github.com/Starou/django-stachoutils',
-    long_description = README,
-    packages=[
-        'django_stachoutils', 
-        'django_stachoutils.management', 
-        'django_stachoutils.management.commands', 
-        'django_stachoutils.forms', 
-        'django_stachoutils.views', 
-        'django_stachoutils.templatetags', 
-    ],
-    package_data={
-        'django_stachoutils': [
-            'static/django_stachoutils/css/*.css',
-            'static/django_stachoutils/js/*.js',
-            'static/django_stachoutils/img/*.png',
-            'static/django_stachoutils/img/*.jpg',
-            'static/django_stachoutils/img/*.gif',
-            'templates/django_stachoutils/*.html',
-        ]
-    },
+    long_description=README,
+    include_package_data=True,
+    packages=find_packages(),
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -45,5 +30,5 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
+    ]
 )
