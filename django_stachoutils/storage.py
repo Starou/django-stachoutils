625242016d4d1deb0ae7949585b958c3ba91b16f
Stanislas Guerra <stanislas.guerra@gmail.com>
2014-05-27 10:39:46 +0200

    OverwriteStorage ne dépend plus de sorl.delete().

--- django_stachoutils/storage.py
+++ django_stachoutils/storage.py
@@ -1,15 +1,12 @@
 import os
-
 from django.core.files.storage import FileSystemStorage
-from django.core.files import File
-from sorl.thumbnail import delete
 
 
 class OverwriteStorage(FileSystemStorage):
     def _save(self, name, content):
         if self.exists(name):
-            img = File(open(os.path.join(self.location, name), "w"))
-            delete(img)
+            os.unlink(os.path.join(self.location, name))
         return super(OverwriteStorage, self)._save(name, content)
+
     def get_available_name(self, name):
         return name
