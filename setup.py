0536b3f95b4995dc2598f84fc01af741ef3b764f
Stanislas Guerra <stanislas.guerra@gmail.com>
2014-06-12 12:13:48 +0200

    Fix processing.png size. Bump version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-stachoutils",
-    version="0.92.0",
+    version="0.92.1",
     license='BSD Licence',
     author='Stanislas Guerra',
     author_email='stan@slashdev.me',
