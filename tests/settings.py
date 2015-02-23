5e6a75f7d5766605bf39b1a6b36270ba1d557d47
Stanislas Guerra <stanislas.guerra@gmail.com>
2015-02-23 14:16:53 +0100

    Add tests.settings.

---
+++ tests/settings.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'i0_+-t@@wul&q)30+4y)8-19s)31@%cv8$q(c@8q1g#h$6wn-='
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+MEDIA_URL = 'http://media.my_project.com/'
+MEDIA_ROOT = '/media'
