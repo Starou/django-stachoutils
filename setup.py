37df3c9523575e24ed5335539e35175e969d7fcc
Stanislas Guerra <sguerra@figarocms.fr>
2017-07-17 14:04:27 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-stachoutils",
-    version="2.2.0",
+    version="2.2.1",
     license='BSD Licence',
     author='Stanislas Guerra',
     author_email='stan@slashdev.me',
