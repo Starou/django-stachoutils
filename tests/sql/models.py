92cfb6d47fcb4aac28241f0ed12cb99b20f416a7
Stanislas Guerra <sguerra@figarocms.fr>
2018-03-07 16:18:21 +0100

    Add a test on CountCase that query a database.

---
+++ tests/sql/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
