bda0f57ba5b25bbc0d8b0b2e99939a4d46bd295c
Stanislas Guerra <sguerra@figarocms.fr>
2018-03-07 16:43:42 +0100

    Add SumCase test.

--- tests/sql/models.py
+++ tests/sql/models.py
@@ -6,6 +6,7 @@ class Car(models.Model):
     brand = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     top_speed = models.IntegerField()
+    sales = models.IntegerField()
 
     def __str__(self):
         return self.name
--- tests/sql/tests.py
+++ tests/sql/tests.py
@@ -8,15 +8,15 @@ class AggregateTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.c1 = Car.objects.create(brand=u"Saab", name=u"9-3 2.0T",
-                                    type=u"Coupé", top_speed=230)
+                                    type=u"Coupé", top_speed=230, sales=150000)
         cls.c2 = Car.objects.create(brand=u"Saab", name=u"900 Turbo16",
-                                    type=u"Coupé", top_speed=215)
+                                    type=u"Coupé", top_speed=215, sales=225000)
         cls.c3 = Car.objects.create(brand=u"Alfa Roméo", name=u"Giulietta 2.0",
-                                    type=u"Sedan", top_speed=185)
+                                    type=u"Sedan", top_speed=185, sales=115000)
         cls.c4 = Car.objects.create(brand=u"Alfa Roméo", name=u"GTV",
-                                    type=u"Coupé", top_speed=195)
+                                    type=u"Coupé", top_speed=195, sales=65000)
         cls.c5 = Car.objects.create(brand=u"Lada", name=u"Niva",
-                                    type=u"4x4", top_speed=150)
+                                    type=u"4x4", top_speed=150, sales=300000)
 
     def test_count_case(self):
         from django_stachoutils.sql import CountCase
@@ -29,3 +29,11 @@ class AggregateTestCase(TestCase):
                                                                  when=u"top_speed > 190",
                                                                  quote=""))
         self.assertEqual(qset['nb_rapides'], 3)
+
+    def test_sum_case(self):
+        from django_stachoutils.sql import SumCase
+        qset = Car.objects.all().aggregate(
+            nb_ventes_coupes=SumCase('sales',
+                                     case_expression="type",
+                                     when=u"Coupé"))
+        self.assertEqual(qset['nb_ventes_coupes'], 440000)
