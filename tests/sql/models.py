5221982a10992523e0484959779a7ce6e323196a
Stanislas Guerra <sguerra@figarocms.fr>
2018-03-07 16:28:30 +0100

    Add test on CountCase2.

--- tests/sql/models.py
+++ tests/sql/models.py
@@ -5,6 +5,7 @@ class Car(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
+    top_speed = models.IntegerField()
 
     def __str__(self):
         return self.name
