c6b753fc45f3c59e3e49ad9f84a545122f8213bf
Stanislas Guerra <sguerra@figarocms.fr>
2018-03-13 11:38:44 +0100

    Add log.MessagelHandler test.

--- tests/settings.py
+++ tests/settings.py
@@ -20,3 +20,50 @@ MEDIA_ROOT = os.path.join(CURRENT_DIR, 'media')
 
 THUMBOR_SERVER = 'http://my_thumbor.com:8888'
 THUMBOR_SERVER_EXTERNAL = THUMBOR_SERVER
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/tmp/django_mail'
+#EMAIL_HOST = 'smtp'
+EMAIL_SUBJECT_PREFIX = "[DJANGO PROJECT] "
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(asctime)s %(message)s',
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'ERROR',
+            'class': 'django_stachoutils.log.NullHandler',
+        },
+        'message': {
+            'level': 'WARNING',
+            'class': 'django_stachoutils.log.MessagelHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins_buffered': {
+            'level': 'INFO',
+            'class': 'django_stachoutils.log.BufferingAdminEmailHandler',
+            'subject': "[MY PROJECT]",
+        },
+        'mail_admins': {
+            'level': 'INFO',
+            'class': 'django_stachoutils.log.AdminEmailHandler',
+            'subject': "[MY PROJECT]",
+        },
+    },
+    'loggers': {
+        'django.DisallowedHost': {
+            'handlers': ['null'],
+            'propagate': True,
+        },
+        'stachoutils.logger1': {
+            'handlers': ['message'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
