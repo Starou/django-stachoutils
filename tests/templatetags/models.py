cacfcafc461b5eb98aa7ab048905d04e2b540b94
Stanislas Guerra <sguerra@figarocms.fr>
2018-03-14 16:17:52 +0100

    Add tests for table tags.

---
+++ tests/templatetags/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return "cars/{}/".format(self.pk)
+
+    def price_html(self):
+        return u'<em>{}</em>€'.format(self.price)
+    price_html.allow_tags = True
+    price.short_description = 'price'
---
+++ tests/templatetags/test_tags.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+from django.template.engine import Engine
+from django.test import TestCase
+from functools import wraps
+
+from .models import Car
+
+
+def set_templates(templates):
+    templates = dict((name, '{% load stachoutils %}' + html) for name, html in templates.items())
+    loaders = [
+        ('django.template.loaders.cached.Loader', [
+            ('django.template.loaders.locmem.Loader', templates),
+        ]),
+    ]
+    engine = Engine(
+        libraries={
+           'stachoutils': 'django_stachoutils.templatetags.stachoutils_extras',
+        },
+        loaders=loaders,
+    )
+    def decorator(f):
+        @wraps(f)
+        def wrapper(self):
+            self.engine = engine
+            f(self)
+        return wrapper
+    return decorator
+
+
+class TableTagTestCase(TestCase):
+    column = [
+        {'label': 'Name', 'field': 'name', 'link': {'href': 'get_absolute_url'}},
+        {'label': 'Brand', 'field': 'brand'},
+        {'label': 'Price', 'field': 'price_html'},
+    ]
+
+    @set_templates({'template1': '{% table_header_tag columns full_path %}'})
+    def test_table_header_tag(self):
+        result = self.engine.render_to_string('template1', {
+            'columns': self.column,
+            'full_path': "/cars?sort=price&order=asc"})
+        self.assertHTMLEqual(result, """
+            <th class="action-checkbox-column"> <input id="action-toggle" type="checkbox" style="display: inline;"></th>
+            <th><a href="?sort=price&ot=asc&order=asc&o=0">Name</a><input class="fieldname" type="hidden" value="name"/></th>
+            <th><a href="?sort=price&ot=asc&order=asc&o=1">Brand</a><input class="fieldname" type="hidden" value="brand"/></th>
+            <th><a href="?sort=price&ot=asc&order=asc&o=2">Price</a><input class="fieldname" type="hidden" value="price_html"/></th>
+        """)
+
+    @set_templates({'template1': '{% table_row_tag columns car %}'})
+    def test_table_row_tag(self):
+        car = Car.objects.create(brand='Saab', name='9.3 2.0t', price=12500)
+        result = self.engine.render_to_string('template1', {
+            'columns': self.column,
+            'car': car})
+        self.assertHTMLEqual(result, """
+            <td class="pk"><input type="checkbox" name="_selected_action" value="1" class="action-select"></td>
+            <td class=""><a title="" href="cars/1/">9.3 2.0t</a></td> <td class="">Saab</td> <td class=""><em>12500</em>€</td>
+        """)
