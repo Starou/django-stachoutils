973e127c41aa66fd0b1cb3d313fe3a2b997b9a67
Stanislas Guerra <sguerra@figarocms.fr>
2018-03-15 14:07:46 +0100

    Test 'tags' parameter in table_row_tag.

--- tests/templatetags/models.py
+++ tests/templatetags/models.py
@@ -8,6 +8,7 @@ class Car(models.Model):
     brand = models.CharField(max_length=100)
     price = models.IntegerField()
     purchased_on = models.DateField()
+    for_sale = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
--- tests/templatetags/test_tags.py
+++ tests/templatetags/test_tags.py
@@ -31,12 +31,17 @@ def set_templates(templates):
     return decorator
 
 
+def simple_bool_tag(value):
+    return "yes" if value else "Hell No!"
+
+
 class TableTagTestCase(TestCase):
     column = [
         {'label': 'Name', 'field': 'name', 'link': {'href': 'get_absolute_url'}},
         {'label': 'Brand', 'field': 'brand'},
         {'label': 'Price', 'field': 'price_html'},
         {'label': 'Purchased Year', 'field': 'purchased_on', 'filters': [('date', 'Y')]},
+        {'label': 'For Sale', 'field': 'for_sale', 'tags': [simple_bool_tag]},
     ]
 
     @set_templates({'template1': '{% table_header_tag columns full_path %}'})
@@ -51,11 +56,13 @@ class TableTagTestCase(TestCase):
             <th><a href="?%s">Brand</a><input class="fieldname" type="hidden" value="brand"/></th>
             <th><a href="?%s">Price</a><input class="fieldname" type="hidden" value="price_html"/></th>
             <th><a href="?%s">Purchased Year</a><input class="fieldname" type="hidden" value="purchased_on" /></th>
+            <th><a href="?%s">For Sale</a><input class="fieldname" type="hidden" value="for_sale" /></th>
         """ % (
             urlencode([('o', '0'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
             urlencode([('o', '1'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
             urlencode([('o', '2'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
             urlencode([('o', '3'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
+            urlencode([('o', '4'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
         ))
 
     @set_templates({'template1': '{% table_header_tag columns full_path %}'})
@@ -70,11 +77,13 @@ class TableTagTestCase(TestCase):
             <th class="ascending sorted"><a href="?%s">Brand</a><input class="fieldname" type="hidden" value="brand"/></th>
             <th><a href="?%s">Price</a><input class="fieldname" type="hidden" value="price_html"/></th>
             <th><a href="?%s">Purchased Year</a><input class="fieldname" type="hidden" value="purchased_on" /></th>
+            <th><a href="?%s">For Sale</a><input class="fieldname" type="hidden" value="for_sale" /></th>
         """ % (
             urlencode([('o', '0'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
             urlencode([('o', '1'), ('ot', 'desc'), ('paginate_by', '25'), ('theme', 'dark')]),
             urlencode([('o', '2'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
             urlencode([('o', '3'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
+            urlencode([('o', '4'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
         ))
 
     @set_templates({'template1': '{% table_header_tag columns full_path %}'})
@@ -89,16 +98,20 @@ class TableTagTestCase(TestCase):
             <th class="descending sorted"><a href="?%s">Brand</a><input class="fieldname" type="hidden" value="brand"/></th>
             <th><a href="?%s">Price</a><input class="fieldname" type="hidden" value="price_html"/></th>
             <th><a href="?%s">Purchased Year</a><input class="fieldname" type="hidden" value="purchased_on" /></th>
+            <th><a href="?%s">For Sale</a><input class="fieldname" type="hidden" value="for_sale" /></th>
         """ % (
             urlencode([('o', '0'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
             urlencode([('o', '1'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
             urlencode([('o', '2'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
             urlencode([('o', '3'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
+            urlencode([('o', '4'), ('ot', 'asc'), ('paginate_by', '25'), ('theme', 'dark')]),
         ))
 
     @set_templates({'template1': '{% table_row_tag columns car %}'})
     def test_table_row_tag(self):
-        car = Car.objects.create(brand='Saab', name='9.3 2.0t', price=12500, purchased_on=datetime.date(2015, 7, 29))
+        car = Car.objects.create(brand='Saab', name='9.3 2.0t', price=12500,
+                                 purchased_on=datetime.date(2015, 7, 29),
+                                 for_sale=True)
         result = self.engine.render_to_string('template1', {
             'columns': self.column,
             'car': car})
@@ -108,4 +121,5 @@ class TableTagTestCase(TestCase):
             <td class="">Saab</td>
             <td class=""><em>12500</em>€</td>
             <td class="">2015</td>
+            <td class="">yes</td>
         """)
