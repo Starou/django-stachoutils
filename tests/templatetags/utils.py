704dc706c89d3ffabc2da203b81052a8992f7fa5
Stanislas Guerra <sguerra@figarocms.fr>
2018-03-16 10:31:19 +0100

    Add test for pagination tag.

--- tests/templatetags/utils.py
+++ tests/templatetags/utils.py
@@ -3,15 +3,17 @@ from functools import wraps
 
 
 def set_templates(templates):
-    templates = dict((name, '{% load stachoutils %}' + html) for name, html in templates.items())
+    templates = dict((name, '{% load stachoutils_extras i18n %}' + html) for name, html in templates.items())
     loaders = [
         ('django.template.loaders.cached.Loader', [
             ('django.template.loaders.locmem.Loader', templates),
         ]),
+        'django.template.loaders.app_directories.Loader',
     ]
     engine = Engine(
         libraries={
-           'stachoutils': 'django_stachoutils.templatetags.stachoutils_extras',
+           'stachoutils_extras': 'django_stachoutils.templatetags.stachoutils_extras',
+           'i18n': 'django.templatetags.i18n',
         },
         loaders=loaders,
     )
