e94f13f17092a65c1962cd175db78573724cde33
Stanislas Guerra <sguerra@figarocms.fr>
2018-03-16 16:39:38 +0100

    Test queryset filtering in generic_view.

--- tests/views/test_generic_view.py
+++ tests/views/test_generic_view.py
@@ -54,4 +54,49 @@ class GenericListTestCase(TestCase):
                   <button value="0" name="index" title="Execute the selected action" class="button" type="submit">Execute</button>
                 </div>
               </form>
+              <div>
+                <div>Alfa-Romeo - Giullia</div>
+                <div>Alfa-Romeo - Sprint</div>
+                <div>Saab - 9.3</div>
+                <div>Saab - 9.5</div>
+                <div>Saab - 900</div>
+                <div>Subaru - Forester</div>
+                <div>Subaru - Impreza</div>
+              <div>
+            </html>""")
+
+
+    def test_filtering_generic_list(self):
+        request = self.rf.get('/cars?page=2&q=alfa')
+        request.user = self.user
+        columns = [
+            {'label': 'Name', 'field': 'name', 'link': {'href': 'get_absolute_url'}},
+            {'label': 'Brand', 'field': 'brand'},
+            {'label': 'Price', 'field': 'price_html'},
+            {'label': 'Purchased Year', 'field': 'purchased_on', 'filters': [('date', 'Y')]},
+            {'label': 'For Sale', 'field': 'for_sale'},
+        ]
+        actions = [sell_cars]
+        search = [('Name', 'name__icontains'), ('Brand', 'brand__icontains')]
+        response = generic.generic_list(request, self.queryset, columns, 'generic_list_test.html',
+                                        Car, ClassAdmin=CarAdmin, actions=actions, search=search)
+        self.assertHTMLEqual(response.content.decode('utf8'), """
+            <html>
+              <form action="" method="post">
+                <div class="actions">
+                  <label>Action&nbsp;:
+                    <select name="action">
+                      <option selected="selected" value="">---------</option>
+                      <optgroup label="autres">
+                        <option value="sell_cars">Sell the cars</option>
+                      </optgroup>
+                    </select>
+                  </label>
+                  <button value="0" name="index" title="Execute the selected action" class="button" type="submit">Execute</button>
+                </div>
+              </form>
+              <div>
+                <div>Alfa-Romeo - Giullia</div>
+                <div>Alfa-Romeo - Sprint</div>
+              <div>
             </html>""")
