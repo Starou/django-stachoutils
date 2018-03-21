714995c7b9cd5a7b5b91ef322ee716cec9ba8197
Stanislas Guerra <sguerra@figarocms.fr>
2018-03-21 10:53:56 +0100

    Test files_are_equal() when size are equals.

--- tests/utils/tests.py
+++ tests/utils/tests.py
@@ -15,7 +15,15 @@ class UtilsTestCase(TestCase):
         f1 = ContentFile(open(image1, 'rb').read())
         f2 = ContentFile(open(image2, 'rb').read())
 
+        # Files are differents by size
         self.assertFalse(files_are_equal(f1, f2))
+        self.assertNotEqual(f1.size, f2.size)
 
+        # Files are equals
         f1bis = ContentFile(open(image1, 'rb').read())
         self.assertTrue(files_are_equal(f1, f1bis))
+
+        # Files are differents by content
+        f1_altered = ContentFile(open(image1, 'rb').read().replace(b'\x14', b'\xff'))
+        self.assertFalse(files_are_equal(f1, f1_altered))
+        self.assertEqual(f1.size, f1_altered.size)
