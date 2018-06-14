929a87e97094282c89667e027959f9d83fed664a
Stanislas Guerra <sguerra@figarocms.fr>
2018-06-14 14:10:37 +0200

    Ajoute fonction pour passer itérativement les premières phrases en gras dans le widget TexteEditeur.

--- django_stachoutils/static/django_stachoutils/js/editeur.js
+++ django_stachoutils/static/django_stachoutils/js/editeur.js
@@ -1,6 +1,29 @@
 var editeur = {
-    rxGraisse: /(\w)(\[.*?\]){0,1}(.+?\.)(.*)/m,
-    graissePremierePhrase: function(texte) {
-    }
+    rxGraisse: /(\w\s*)((\[.*?)(\])){0,1}(.+?\.){0,1}(.*)/m,
+    graissePhraseSuivante: function(texte) {
+        var texte = texte.match(editeur.rxGraisse);
+        texte.splice(0, 1);  // Supprime le premier item qui est le texte passé en paramètre.
 
+        // La première phrase n'est pas en gras.
+        if (texte[1] === undefined) {
+            texte.splice(1, 1, "[");
+            texte.splice(5, 0, "]");
+        } else if (texte[4] === undefined) {
+            return texte.input;  // Tout le texte est déjà en gras, on ne change rien.
+        } else {
+            texte.splice(1, 1);      // Supprime le texte en gras.
+            texte.splice(2, 1);      // Supprime le "]"
+            texte.splice(3, 0, "]"); // et le pose à la fin de la phrase suivante.
+        }
+        return texte.join('');
+    }
 };
+
+$(document).ready(function () {
+    $(".editeur").each(function () {
+        var textArea = $(this).find("textarea");
+        $(this).children(".toolbar").children("button.gras_phrases").click(function() {
+            textArea.val(editeur.graissePhraseSuivante(textArea.val()));
+        });
+    });
+});
--- tests/javascript/tests/test_widgets.js
+++ tests/javascript/tests/test_widgets.js
@@ -1,8 +1,12 @@
+/* editeur.rxGraisse */
+
 QUnit.test( "Teste regex graisse du texte", function(assert) {
   var texte = "Paris. Au grand calme et noyé dans la verdure magnifique mas provençal d’environ 390 m{2} sur un hectare de terrain joliment arboré avec piscine sécurisée, pool-house, enclos chevaux. Cette propriété est composée d'une habitation principale d'environ 150 m{2} sur deux niveaux exposée sud donnant sur une terrasse ombragée, de 5 chambres d'hôtes intégralement rénovées avec goût, d'un gîte Type 3 d'environ 50 m{2}, d'une cuisine d'été équipée avec sa chambre et sa salle d’eau indépendante de la bâtisse principale.";
   assert.deepEqual(texte.match(editeur.rxGraisse), [
     "Paris. Au grand calme et noyé dans la verdure magnifique mas provençal d’environ 390 m{2} sur un hectare de terrain joliment arboré avec piscine sécurisée, pool-house, enclos chevaux. Cette propriété est composée d'une habitation principale d'environ 150 m{2} sur deux niveaux exposée sud donnant sur une terrasse ombragée, de 5 chambres d'hôtes intégralement rénovées avec goût, d'un gîte Type 3 d'environ 50 m{2}, d'une cuisine d'été équipée avec sa chambre et sa salle d’eau indépendante de la bâtisse principale.",
     "P",
+    undefined,
+    undefined,
     undefined,
     "aris.",
     " Au grand calme et noyé dans la verdure magnifique mas provençal d’environ 390 m{2} sur un hectare de terrain joliment arboré avec piscine sécurisée, pool-house, enclos chevaux. Cette propriété est composée d'une habitation principale d'environ 150 m{2} sur deux niveaux exposée sud donnant sur une terrasse ombragée, de 5 chambres d'hôtes intégralement rénovées avec goût, d'un gîte Type 3 d'environ 50 m{2}, d'une cuisine d'été équipée avec sa chambre et sa salle d’eau indépendante de la bâtisse principale."
@@ -14,7 +18,22 @@ QUnit.test( "Teste regex graisse du texte", function(assert) {
     "P[aris]. Au grand calme et noyé dans la verdure magnifique mas provençal d’environ 390 m{2} sur un hectare de terrain joliment arboré avec piscine sécurisée, pool-house, enclos chevaux. Cette propriété est composée d'une habitation principale d'environ 150 m{2} sur deux niveaux exposée sud donnant sur une terrasse ombragée, de 5 chambres d'hôtes intégralement rénovées avec goût, d'un gîte Type 3 d'environ 50 m{2}, d'une cuisine d'été équipée avec sa chambre et sa salle d’eau indépendante de la bâtisse principale.",
     "P",
     "[aris]",
+    "[aris",
+    "]",
     ". Au grand calme et noyé dans la verdure magnifique mas provençal d’environ 390 m{2} sur un hectare de terrain joliment arboré avec piscine sécurisée, pool-house, enclos chevaux.",
     " Cette propriété est composée d'une habitation principale d'environ 150 m{2} sur deux niveaux exposée sud donnant sur une terrasse ombragée, de 5 chambres d'hôtes intégralement rénovées avec goût, d'un gîte Type 3 d'environ 50 m{2}, d'une cuisine d'été équipée avec sa chambre et sa salle d’eau indépendante de la bâtisse principale."
   ], "Match a text with bold tags.");
+});
+
+/* editeur.graissePhraseSuivante() */
+
+QUnit.test( "Teste la fonction qui passe le texte en gras de façon itérative", function(assert) {
+  var texte = "Paris. Au grand calme et noyé dans la verdure magnifique mas provençal d’environ 390 m{2} sur un hectare de terrain joliment arboré avec piscine sécurisée, pool-house, enclos chevaux. Cette propriété est composée d'une habitation principale d'environ 150 m{2} sur deux niveaux exposée sud donnant sur une terrasse ombragée, de 5 chambres d'hôtes intégralement rénovées avec goût, d'un gîte Type 3 d'environ 50 m{2}, d'une cuisine d'été équipée avec sa chambre et sa salle d’eau indépendante de la bâtisse principale.";
+  assert.deepEqual(editeur.graissePhraseSuivante(texte), "P[aris.] Au grand calme et noyé dans la verdure magnifique mas provençal d’environ 390 m{2} sur un hectare de terrain joliment arboré avec piscine sécurisée, pool-house, enclos chevaux. Cette propriété est composée d'une habitation principale d'environ 150 m{2} sur deux niveaux exposée sud donnant sur une terrasse ombragée, de 5 chambres d'hôtes intégralement rénovées avec goût, d'un gîte Type 3 d'environ 50 m{2}, d'une cuisine d'été équipée avec sa chambre et sa salle d’eau indépendante de la bâtisse principale.", "Match a text without bold tags.");
+
+  texte = "P[aris.] Au grand calme et noyé dans la verdure magnifique mas provençal d’environ 390 m{2} sur un hectare de terrain joliment arboré avec piscine sécurisée, pool-house, enclos chevaux. Cette propriété est composée d'une habitation principale d'environ 150 m{2} sur deux niveaux exposée sud donnant sur une terrasse ombragée, de 5 chambres d'hôtes intégralement rénovées avec goût, d'un gîte Type 3 d'environ 50 m{2}, d'une cuisine d'été équipée avec sa chambre et sa salle d’eau indépendante de la bâtisse principale.";
+  assert.deepEqual(editeur.graissePhraseSuivante(texte), "P[aris. Au grand calme et noyé dans la verdure magnifique mas provençal d’environ 390 m{2} sur un hectare de terrain joliment arboré avec piscine sécurisée, pool-house, enclos chevaux.] Cette propriété est composée d'une habitation principale d'environ 150 m{2} sur deux niveaux exposée sud donnant sur une terrasse ombragée, de 5 chambres d'hôtes intégralement rénovées avec goût, d'un gîte Type 3 d'environ 50 m{2}, d'une cuisine d'été équipée avec sa chambre et sa salle d’eau indépendante de la bâtisse principale.", "Match a text without bold tags.");
+
+  texte = "P[aris. Au grand calme et noyé dans la verdure magnifique mas provençal d’environ 390 m{2} sur un hectare de terrain joliment arboré avec piscine sécurisée, pool-house, enclos chevaux.] Cette propriété est composée d'une habitation principale d'environ 150 m{2} sur deux niveaux exposée sud donnant sur une terrasse ombragée, de 5 chambres d'hôtes intégralement rénovées avec goût, d'un gîte Type 3 d'environ 50 m{2}, d'une cuisine d'été équipée avec sa chambre et sa salle d’eau indépendante de la bâtisse principale.";
+  assert.deepEqual(editeur.graissePhraseSuivante(texte), "P[aris. Au grand calme et noyé dans la verdure magnifique mas provençal d’environ 390 m{2} sur un hectare de terrain joliment arboré avec piscine sécurisée, pool-house, enclos chevaux. Cette propriété est composée d'une habitation principale d'environ 150 m{2} sur deux niveaux exposée sud donnant sur une terrasse ombragée, de 5 chambres d'hôtes intégralement rénovées avec goût, d'un gîte Type 3 d'environ 50 m{2}, d'une cuisine d'été équipée avec sa chambre et sa salle d’eau indépendante de la bâtisse principale.]", "Match a text without bold tags.");
 });
