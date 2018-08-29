1ee91dc481cb4374b2caa0df590319983ea17aed
Stanislas Guerra <sguerra@figarocms.fr>
2018-08-29 13:33:10 +0200

    Fix backward compat import.

--- django_stachoutils/forms/__init__.py
+++ django_stachoutils/forms/__init__.py
@@ -2,5 +2,5 @@
 
 from .fields import ImageModelChoiceField
 from .models import ModelFormOptions, ModelFormMetaclass, ModelForm
-from .nested import NestedModelFormOptions, NestedModelFormMetaclass, NestedModelForm
+from .nested import NestedModelFormOptions, NestedModelFormMetaclass, NestedModelForm, NESTED_NON_FIELD_ERRORS
 from .widgets import ImageDroppableHiddenInput, get_thumbor_thumbnail_url, get_thumbor_thumbnail_tag
