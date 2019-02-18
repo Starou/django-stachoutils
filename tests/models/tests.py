014581367de4f203717d3c640f2c50555141418e
Stanislas Guerra <stan@slashdev.fr>
2019-02-18 14:48:22 +0100

    cached instance in models.get_obj_dict() has been removed in Django 2.

--- tests/models/tests.py
+++ tests/models/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django import VERSION as DJ_VERSION
 from django_stachoutils import models
 from .models import Car, Person
 
@@ -7,10 +8,13 @@ class ModelsTest(TestCase):
     def test_get_obj_dict(self):
         person = Person.objects.create(name='Stan', lastname='Guerra', gender='male')
         car = Car.objects.create(brand='Saab', name='9.3', owner=person)
-        self.assertEqual(models.get_obj_dict(car), {
-            '_owner_cache': person,
+        expected = {
             'brand': 'Saab',
             'description': None,
             'name': '9.3',
-            'owner_id': person.pk}
-        )
+            'owner_id': person.pk
+        }
+        if DJ_VERSION < (2, 0):
+            expected['_owner_cache'] = person
+
+        self.assertEqual(models.get_obj_dict(car), expected)
