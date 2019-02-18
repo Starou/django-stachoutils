ac5715a99df88841fd94640249bb63dd317ed21b
Stanislas Guerra <stan@slashdev.fr>
2019-02-18 14:38:22 +0100

    Add on_delete=models.CASCADE parameter for FK.

--- tests/forms/models.py
+++ tests/forms/models.py
@@ -5,14 +5,14 @@ class Car(models.Model):
     name = models.CharField(max_length=20)
     brand = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
-    owner = models.ForeignKey('Person', null=True)
+    owner = models.ForeignKey('Person', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
 
 
 class CarOption(models.Model):
-    car = models.ForeignKey('Car')
+    car = models.ForeignKey('Car', on_delete=models.CASCADE)
     name = models.CharField(max_length=15)
 
 
@@ -20,7 +20,7 @@ class Person(models.Model):
     name = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     gender = models.CharField(choices=[('male', 'Male'), ('female', 'Female')], max_length=6)
-    house = models.ForeignKey('House', null=True)
+    house = models.ForeignKey('House', on_delete=models.CASCADE, null=True)
 
 
 def house_photo_path(instance, filename):
