4257ae7ae9df6952dc8da01dd473fa8e367b771e
Stanislas Guerra <sguerra@slashdev.fr>
2020-06-10 13:52:13 +0200

    Fix dumpdata_related command for Django 3.
    'Field.rel' has been replaced with 'Field.remote_field'.
    Use built-in sort_dependencies().

--- django_stachoutils/management/commands/dumpdata_related.py
+++ django_stachoutils/management/commands/dumpdata_related.py
@@ -68,9 +68,10 @@ class Command(BaseCommand):
         if len(app_labels) == 0:
             if primary_keys:
                 raise CommandError("You can only use --pks option with one model")
-            app_list = OrderedDict((app_config, None)
-                for app_config in apps.get_app_configs()
-                if app_config.models_module is not None and app_config not in excluded_apps)
+            app_list = OrderedDict(
+                (app_config, None) for app_config in apps.get_app_configs()
+                if app_config.models_module is not None and app_config not in excluded_apps
+            )
         else:
             if len(app_labels) > 1 and primary_keys:
                 raise CommandError("You can only use --pks option with one model")
@@ -111,7 +112,7 @@ class Command(BaseCommand):
                     app_list[app_config] = None
 
         if related:
-            if len(app_list) != 1 or len(app_list.values()[0]) != 1:
+            if len(app_list) != 1 or len(list(app_list.values())[0]) != 1:
                 raise CommandError("you can only use --related option with one model")
 
         # Check that the serialization format exists; this is a shortcut to
@@ -126,7 +127,8 @@ class Command(BaseCommand):
 
         def get_objects():
             # Collate the objects to be serialized.
-            for model in sort_dependencies(app_list.items()):
+            models = serializers.sort_dependencies(app_list.items())
+            for model in models:
                 if model in excluded_models:
                     continue
                 if not model._meta.proxy and router.allow_migrate(using, model):
@@ -172,7 +174,7 @@ class Command(BaseCommand):
                         # dependency order.
                         rel_obj = rel_objs[0]
                         for field in rel_obj._meta.fields:
-                            if field.rel and field.rel.to == obj.__class__ and \
+                            if field.remote_field and field.remote_field.model == obj.__class__ and \
                                getattr(rel_obj, field.name) == obj:
                                 rel_objs.insert(0, obj)
                                 break
@@ -189,85 +191,10 @@ class Command(BaseCommand):
         try:
             self.stdout.ending = None
             serializers.serialize(format, get_objects(), indent=indent,
-                    use_natural_foreign_keys=use_natural_foreign_keys,
-                    use_natural_primary_keys=use_natural_primary_keys,
-                    stream=open(output, 'w') if output else self.stdout)
+                                  use_natural_foreign_keys=use_natural_foreign_keys,
+                                  use_natural_primary_keys=use_natural_primary_keys,
+                                  stream=open(output, 'w') if output else self.stdout)
         except Exception as e:
             if show_traceback:
                 raise
             raise CommandError("Unable to serialize database: %s" % e)
-
-
-def sort_dependencies(app_list):
-    """Sort a list of (app_config, models) pairs into a single list of models.
-
-    The single list of models is sorted so that any model with a natural key
-    is serialized before a normal model, and any model with a natural key
-    dependency has it's dependencies serialized first.
-    """
-    # Process the list of models, and get the list of dependencies
-    model_dependencies = []
-    models = set()
-    for app_config, model_list in app_list:
-        if model_list is None:
-            model_list = app_config.get_models()
-
-        for model in model_list:
-            models.add(model)
-            # Add any explicitly defined dependencies
-            if hasattr(model, 'natural_key'):
-                deps = getattr(model.natural_key, 'dependencies', [])
-                if deps:
-                    deps = [apps.get_model(dep) for dep in deps]
-            else:
-                deps = []
-
-            # Now add a dependency for any FK or M2M relation with
-            # a model that defines a natural key
-            for field in model._meta.fields:
-                if hasattr(field.rel, 'to'):
-                    rel_model = field.rel.to
-                    if hasattr(rel_model, 'natural_key') and rel_model != model:
-                        deps.append(rel_model)
-            for field in model._meta.many_to_many:
-                rel_model = field.rel.to
-                if hasattr(rel_model, 'natural_key') and rel_model != model:
-                    deps.append(rel_model)
-            model_dependencies.append((model, deps))
-
-    model_dependencies.reverse()
-    # Now sort the models to ensure that dependencies are met. This
-    # is done by repeatedly iterating over the input list of models.
-    # If all the dependencies of a given model are in the final list,
-    # that model is promoted to the end of the final list. This process
-    # continues until the input list is empty, or we do a full iteration
-    # over the input models without promoting a model to the final list.
-    # If we do a full iteration without a promotion, that means there are
-    # circular dependencies in the list.
-    model_list = []
-    while model_dependencies:
-        skipped = []
-        changed = False
-        while model_dependencies:
-            model, deps = model_dependencies.pop()
-
-            # If all of the models in the dependency list are either already
-            # on the final model list, or not on the original serialization list,
-            # then we've found another model with all it's dependencies satisfied.
-            found = True
-            for candidate in ((d not in models or d in model_list) for d in deps):
-                if not candidate:
-                    found = False
-            if found:
-                model_list.append(model)
-                changed = True
-            else:
-                skipped.append((model, deps))
-        if not changed:
-            raise CommandError("Can't resolve dependencies for %s in serialized app list." %
-                ', '.join('%s.%s' % (model._meta.app_label, model._meta.object_name)
-                for model, deps in sorted(skipped, key=lambda obj: obj[0].__name__))
-            )
-        model_dependencies = skipped
-
-    return model_list
