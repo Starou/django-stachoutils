7035295c19bca2cedbfc45a562c11e4e9a3d0850
Stanislas Guerra <stan@slashdev.fr>
2022-04-04 12:33:26 +0200

    Django History: log functions accepts a user directly instead of requests.
    + Still accept request for bw-compat.
    + Add message parameter for log_addition.

--- django_stachoutils/options.py
+++ django_stachoutils/options.py
@@ -7,57 +7,70 @@ from django.utils.encoding import force_str
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 
-def log_addition(request, object):
+def log_addition(request_or_user, obj, message="Creation"):
+    if hasattr(request_or_user, 'user'):
+        user_id = request_or_user.user.pk
+    else:
+        user_id = request_or_user.pk
     LogEntry.objects.log_action(
-        user_id         = request.user.pk,
-        content_type_id = ContentType.objects.get_for_model(object).pk,
-        object_id       = object.pk,
-        object_repr     = force_str(object),
-        action_flag     = ADDITION
+        user_id         = user_id,
+        content_type_id = ContentType.objects.get_for_model(obj).pk,
+        object_id       = obj.pk,
+        object_repr     = force_str(obj),
+        action_flag     = ADDITION,
+        change_message  = message
     )
 
 
-def log_change(request, object, message):
+def log_change(request_or_user, obj, message):
+    if hasattr(request_or_user, 'user'):
+        user_id = request_or_user.user.pk
+    else:
+        user_id = request_or_user.pk
     LogEntry.objects.log_action(
-        user_id         = request.user.pk,
-        content_type_id = ContentType.objects.get_for_model(object).pk,
-        object_id       = object.pk,
-        object_repr     = force_str(object),
+        user_id         = user_id,
+        content_type_id = ContentType.objects.get_for_model(obj).pk,
+        object_id       = obj.pk,
+        object_repr     = force_str(obj),
         action_flag     = CHANGE,
         change_message  = message
     )
 
 
-def log_deletion(request, object, object_repr):
+def log_deletion(request_or_user, obj, object_repr):
+    if hasattr(request_or_user, 'user'):
+        user_id = request_or_user.user.pk
+    else:
+        user_id = request_or_user.pk
     LogEntry.objects.log_action(
-        user_id         = request.user.id,
-        content_type_id = ContentType.objects.get_for_model(object).pk,
-        object_id       = object.pk,
+        user_id         = user_id,
+        content_type_id = ContentType.objects.get_for_model(obj).pk,
+        object_id       = obj.pk,
         object_repr     = object_repr,
         action_flag     = DELETION
     )
 
 
-def log_change_for_user(user, object, message):
+def log_change_for_user(user, obj, message):
     LogEntry.objects.log_action(
         user_id         = user.pk,
-        content_type_id = ContentType.objects.get_for_model(object).pk,
-        object_id       = object.pk,
-        object_repr     = force_str(object),
+        content_type_id = ContentType.objects.get_for_model(obj).pk,
+        object_id       = obj.pk,
+        object_repr     = force_str(obj),
         action_flag     = CHANGE,
         change_message  = message
     )
 
 
-def logs_for_formsets(mode, request, object, forms):
+def logs_for_formsets(mode, request_or_user, obj, forms):
     """forms est une liste de forms ou formsets."""
     if mode == 'addition':
-        log_addition(request, object)
+        log_addition(request_or_user, obj)
     elif mode == 'change':
         msg = u'a modifié : '
         for f in forms:
             msg += _log_for_forms(f)
-        log_change(request, object, msg)
+        log_change(request_or_user, obj, msg)
 
 
 def _log_for_forms(f):
@@ -73,7 +86,6 @@ def _log_for_forms(f):
         for f2 in f.forms:
             msg += _log_for_forms(f2)
     return msg
-
 
 
 def paginate(request, queryset, paginate_by=50):
