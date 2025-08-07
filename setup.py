0628e755e64e8c11cbf80b87babf54c4ce6dadd7
Stanislas Guerra <stan@slashdev.fr>
2025-08-07 12:49:29 +0200

    Bump to 4.7.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-stachoutils",
-    version="4.7.0a1",
+    version="4.7.0",
     license='BSD Licence',
     author='Stanislas Guerra',
     author_email='stan@slashdev.me',
